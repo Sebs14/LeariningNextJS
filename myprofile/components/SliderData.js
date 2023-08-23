08067656a5fe14f1d00dfb332ec93b4d6a40945f
Sebs14 <00196919@uca.edu.sv>
2023-08-22 20:32:04 -0600

    refactor: :fire: Remove some code from a past project
    project list updated and deleted one from the past that has been reworked by other people

--- myprofile/components/SliderData.js
+++ myprofile/components/SliderData.js
@@ -1,26 +1,17 @@
 export const SliderData = [
-    {
-        title: "Vet4Paws",
-        url: "https://veterinary4paws.netlify.app",
-        image: 
-        '/Vet4Paws.png',
-    },
-    {
-        title: "Foodies",
-        url: "https://foodies-elaniin-foodies-web.vercel.app",
-        image: 
-        '/Foodies.png',
-    },
-    {
-        title: "Pokedex",
-        url: "https://elaniin-pokedex-web-five.vercel.app",
-        image: 
-        '/pokedex.png',
-    },
-    {
-        title: "MDUPay",
-        url: "https://pay.mdu.dev",
-        image: 
-        '/MDUPay.png',
-    },
+  {
+    title: "Vet4Paws",
+    url: "https://veterinary4paws.netlify.app",
+    image: "/Vet4Paws.png",
+  },
+  {
+    title: "Foodies",
+    url: "https://foodies-elaniin-foodies-web.vercel.app",
+    image: "/Foodies.png",
+  },
+  {
+    title: "Pokedex",
+    url: "https://elaniin-pokedex-web-five.vercel.app",
+    image: "/pokedex.png",
+  },
 ];
